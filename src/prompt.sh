2f74768fc3897483d463c449f3be0266ff5bf4b8
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-02-08 17:27:17 -0800

    Create basic prompt system

--- src/prompt.sh
+++ src/prompt.sh
@@ -1,72 +1,127 @@
-RESET="\033[0m"
+NEAT_PROMPT=""
 
-BLACK="\033[30m"
-RED="\033[31m"
-GREEN="\033[32m"
-YELLOW="\033[33m"
-BLUE="\033[34m"
-MAGENTA="\033[35m"
-CYAN="\033[36m"
-WHITE="\033[37m"
+neat_prompt_color() {
+    local colors=(
+        "BLACK"
+        "RED"
+        "GREEN"
+        "YELLOW"
+        "BLUE"
+        "MAGENTA"
+        "CYAN"
+        "WHITE"
+        "BRIGHT_BLACK"
+        "BRIGHT_RED"
+        "BRIGHT_GREEN"
+        "BRIGHT_YELLOW"
+        "BRIGHT_BLUE"
+        "BRIGHT_MAGENTA"
+        "BRIGHT_CYAN"
+        "BRIGHT_WHITE"
+        "BG_BLACK"
+        "BG_RED"
+        "BG_GREEN"
+        "BG_YELLOW"
+        "BG_BLUE"
+        "BG_MAGENTA"
+        "BG_CYAN"
+        "BG_WHITE"
+        "BG_BRIGHT_BLACK"
+        "BG_BRIGHT_RED"
+        "BG_BRIGHT_GREEN"
+        "BG_BRIGHT_YELLOW"
+        "BG_BRIGHT_BLUE"
+        "BG_BRIGHT_MAGENTA"
+        "BG_BRIGHT_CYAN"
+        "BG_BRIGHT_WHITE"
+    )
 
-BRIGHT_BLACK="\033[30;1m"
-BRIGHT_RED="\033[31;1m"
-BRIGHT_GREEN="\033[32;1m"
-BRIGHT_YELLOW="\033[33;1m"
-BRIGHT_BLUE="\033[34;1m"
-BRIGHT_MAGENTA="\033[35;1m"
-BRIGHT_CYAN="\033[36;1m"
-BRIGHT_WHITE="\033[37;1m"
+    local color="${1^^}"
+    local var="$"
 
-BG_BLACK="\033[40m"
-BG_RED="\033[41m"
-BG_GREEN="\033[42m"
-BG_YELLOW="\033[43m"
-BG_BLUE="\033[44m"
-BG_MAGENTA="\033[45m"
-BG_CYAN="\033[46m"
-BG_WHITE="\033[47m"
-
-BG_BRIGHT_BLACK="\033[40;1m"
-BG_BRIGHT_RED="\033[41;1m"
-BG_BRIGHT_GREEN="\033[42;1m"
-BG_BRIGHT_YELLOW="\033[43;1m"
-BG_BRIGHT_BLUE="\033[44;1m"
-BG_BRIGHT_MAGENTA="\033[45;1m"
-BG_BRIGHT_CYAN="\033[46;1m"
-BG_BRIGHT_WHITE="\033[47;1m"
-
-git_branch() {
-    local is_repo="$(git rev-parse --is-inside-work-tree 2>&1)"
-    local return_value=""
-
-    if [[ "$is_repo" == "true" ]]; then
-        printf "\001$RED\002$(git branch --show-current)\001$RESET\002"
+    if _neat_contains "$colors" "$color"; then
+        eval "NEAT_PROMPT=\"${NEAT_PROMPT}\001${var}NEAT_${color}\002\""
+    else
+        _neat_error "${color,,} is not a color"
     fi
 }
 
-git_status_2() {
-    local is_repo="$(git rev-parse --is-inside-work-tree 2>&1)"
-
-    if [[ "$is_repo" == "true" ]] && [[ "$(git status --short)" != "" ]]; then
-        printf "\001$RED\002*\001$RESET\002)"
-    elif [[ "$is_repo" == true ]]; then
-        printf ")"
+neat_prompt_text() {
+    if [[ $1 == "" ]]; then
+        _neat_error "no arguments were passed"
+    else
+        NEAT_PROMPT="${NEAT_PROMPT}$1"
     fi
 }
 
-git_status_1() {
-    local is_repo="$(git rev-parse --is-inside-work-tree 2>&1)"
-
-    if [[ "$is_repo" == "true" ]] && [[ "$(git status --short)" != "" ]]; then
-        printf " (\001$RED\002*\001$RESET\002"
-    elif [[ "$is_repo" == true ]]; then
-        printf " ("
-    fi
+neat_prompt_date() {
+    neat_prompt_text "\d"
 }
 
-git_prompt() {
-    printf '$(git_status_1)$(git_branch)$(git_status_2)'
+neat_prompt_hostname() {
+    neat_prompt_text "\H"
 }
 
-export PS1="\[$BRIGHT_GREEN\]\u\[$RESET\] \[$BLUE\]\W\[$RESET\]$(git_prompt) \$ "
+neat_prompt_hostname_short() {
+    neat_prompt_text "\h"
+}
+
+neat_prompt_jobs() {
+    neat_prompt_text "\j"
+}
+
+neat_prompt_basename() {
+    neat_prompt_text "\l"
+}
+
+neat_prompt_shellname() {
+    neat_prompt_text "\s"
+}
+
+neat_prompt_time() {
+    neat_prompt_text "\@"
+}
+
+neat_prompt_time_24() {
+    neat_prompt_text "\t"
+}
+
+neat_prompt_time_12() {
+    neat_prompt_text "\T"
+}
+
+neat_prompt_username() {
+    neat_prompt_text "\u"
+}
+
+neat_prompt_shversion() {
+    neat_prompt_text "\v"
+}
+
+neat_prompt_shrelease() {
+    neat_prompt_text "\V"
+}
+
+neat_prompt_path() {
+    neat_prompt_text "\w"
+}
+
+neat_prompt_folder() {
+    neat_prompt_text "\W"
+}
+
+neat_prompt_histnumber() {
+    neat_prompt_text "\!"
+}
+
+neat_prompt_cmdnumber() {
+    neat_prompt_text "\#"
+}
+
+neat_prompt_prompt() {
+    neat_prompt_text "\$"
+}
+
+neat_prompt_update() {
+    export PS1="$NEAT_PROMPT"
+}
