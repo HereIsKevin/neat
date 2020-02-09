26e0c863ed189ef8ad43a2a6be954bab429a2e7a
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-02-08 18:31:00 -0800

    Create basic plugin system

---
+++ src/plugins.sh
@@ -0,0 +1,15 @@
+NEAT_PLUGINS=()
+
+neat_plugin_load() {
+    local path="$1"
+
+    if [ -d "$path" ]; then
+        NEAT_PLUGINS+="$path"
+    fi
+
+    source "$path/init.sh"
+
+    if [ -f "$path/aliases.sh" ]; then
+        source "$path/aliases.sh"
+    fi
+}
