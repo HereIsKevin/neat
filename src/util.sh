ccb39b18f65515f77ddceb46d1f6472b7f772e4c
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-02-08 17:27:09 -0800

    Create utilities for writing prompt system

---
+++ src/util.sh
@@ -0,0 +1,35 @@
+_neat_contains() {
+    local list="$1"
+    local value="$2"
+    local found="0"
+
+    for item in "${list[@]}"; do
+        if [[ "$item" == "$value" ]]; then
+            found="1"
+            break
+        fi
+    done
+
+    return "$found"
+}
+
+_neat_error() {
+    local quit="1"
+    local message="$1"
+
+    if [[ "$1" == "--no-exit" ]]; then
+        local quit="0"
+        local message="$2"
+    elif [[ "$2" == "--no-exit" ]]; then
+        local quit="0"
+        local message="$1"
+    fi
+
+    printf "error: ${message}"
+
+    if [[ "$quit" == true ]]; then
+        exit "1"
+    else
+        return "1"
+    fi
+}
