9433b1be66bcf8f993e5a344c35c005b9064bf4c
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-02-10 15:44:08 -0800

    Use prompt optimizations in git plugin

--- plugins/git/init.sh
+++ plugins/git/init.sh
@@ -1,3 +1,5 @@
+NEAT_PLUGIN_GIT_STATUS_CACHE=""
+
 _neat_plugin_git_branch() {
     local is_repo="$(git rev-parse --is-inside-work-tree 2>&1)"
 
@@ -9,6 +11,10 @@ _neat_plugin_git_branch() {
 }
 
 neat_plugin_git_branch() {
+    if [[ "$(_neat_contains "NEAT_UPDATE" "neat_status")" == "0" ]]; then
+        NEAT_UPDATE+=("neat_plugin_git_update")
+    fi
+
     NEAT_PROMPT+=("_neat_plugin_git_branch")
 }
 
@@ -23,5 +29,18 @@ _neat_plugin_git_status() {
 }
 
 neat_plugin_git_status() {
+    if [[ "$(_neat_contains "NEAT_UPDATE" "neat_status")" == "0" ]]; then
+        NEAT_UPDATE+=("neat_plugin_git_update")
+    fi
+
     NEAT_PROMPT+=("_neat_plugin_git_status")
+}
+
+neat_plugin_git_update() {
+    local git_status="$(git status)"
+
+    if [[ "$git_status" != "$NEAT_PLUGIN_GIT_STATUS_CACHE" ]]; then
+        NEAT_PLUGIN_GIT_STATUS_CACHE="$git_status"
+        NEAT_UPDATE="true"
+    fi
 }
