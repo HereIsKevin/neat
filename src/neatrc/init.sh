f7509e6317cd679225737c8c85cc2f4c283e49ea
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-02-11 20:04:52 +0000

    Create default neatrc and loader

--- src/neatrc/init.sh
+++ src/neatrc/init.sh
@@ -7,11 +7,19 @@ NEAT_RCS=(
 NEAT_INSTALL="$HOME/.neat/"
 NEAT_BUILTIN="$NEAT/plugins/"
 
+NEAT_SOURCED="false"
+
+
 for file in "${NEAT_RCS[@]}"; do
     if [ -f "$file" ]; then
         source "$file"
+        NEAT_SOURCED="true"
     fi
 done
+
+if [[ "$NEAT_SOURCED" == "false" ]]; then
+    source "$NEAT/neatrc/neatrc.sh"
+fi
 
 source "$NEAT/neatrc/unaliases.sh"
 
---
+++ src/neatrc/neatrc.sh
@@ -0,0 +1,11 @@
+plugin_load "$NEAT_BUILTIN/git/"
+
+color "bright_green"
+username
+color "reset"
+text " "
+color "blue"
+folder
+color "reset"
+text " \$ "
+update
