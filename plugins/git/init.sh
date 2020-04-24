3604f6d81554afe3c37b70fca22acb2778c11c6d
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-04-24 12:38:07 -0700

    Fix git plugin emitting error on new empty repo

--- plugins/git/init.sh
+++ plugins/git/init.sh
@@ -1,5 +1,5 @@
 # =============================git plugin for neat==============================
-# Version 0.1.1
+# Version 0.1.2
 # Copyright (c) 2020 Kevin Feng (https://github.com/HereIsKevin)
 # Licensed under GPLv3 (https://github.com/HereIsKevin/neat/blob/master/LICENSE)
 # ==============================================================================
@@ -8,10 +8,10 @@ NEAT_PLUGIN_GIT_STATUS_CACHE=""
 NEAT_PLUGIN_GIT_BRANCH_CACHE=""
 
 _neat_plugin_git_branch() {
-    local is_repo="$(git rev-parse --is-inside-work-tree 2>&1)"
+    local is_repo="$(git rev-parse --is-inside-work-tree 2> /dev/null)"
 
     if [[ "$is_repo" == "true" ]]; then
-        printf "$(git rev-parse --abbrev-ref HEAD) "
+        printf "$(git rev-parse --abbrev-ref HEAD 2> /dev/null) "
     else
         printf ""
     fi
@@ -26,9 +26,9 @@ neat_plugin_git_branch() {
 }
 
 _neat_plugin_git_status() {
-    local is_repo="$(git rev-parse --is-inside-work-tree 2>&1)"
+    local is_repo="$(git rev-parse --is-inside-work-tree 2> /dev/null)"
 
-    if [[ "$is_repo" == "true" && "$(git status --porcelain)" != "" ]]; then
+    if [[ "$is_repo" == "true" && "$(git status --porcelain 2> /dev/null)" != "" ]]; then
         printf "* "
     else
         printf ""
@@ -44,8 +44,8 @@ neat_plugin_git_status() {
 }
 
 neat_plugin_git_check() {
-    local git_status="$(git status --porcelain 2>&1)"
-    local git_branch="$(git rev-parse --abbrev-ref HEAD 2>&1)"
+    local git_status="$(git status --porcelain 2> /dev/null)"
+    local git_branch="$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"
 
     if [[ "$git_status" != "$NEAT_PLUGIN_GIT_STATUS_CACHE" || \
         "$git_branch" != "$NEAT_PLUGIN_GIT_BRANCH_CACHE" ]]; then
